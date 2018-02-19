708ee78099c7b1545112d2ade10e7dcc38dca93f
Seatis <kezer.attila@mikrum.hu>
2018-02-19 15:51:41 +0100

    feat(routing): routing logic has been implemented

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,17 +11,11 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    console.log(this.selectedHero.id);
   }
 
   getHeroes(): void {
