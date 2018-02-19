000874c4f304f538d6722d041ca383b0490b5f3a
Seatis <kezer.attila@mikrum.hu>
2018-02-19 14:41:29 +0100

    feat(services): hero.service and message.service have been created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,23 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessagesService } from './messages.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [HeroService, MessagesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/hero.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+import { MessagesService } from './messages.service';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+
+  constructor(private messageService: MessagesService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of (HEROES);
+  }
+}
